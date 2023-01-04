91ca51cf77cffa98a4b87b2afb3a16a88c6ed610
Eric dos Santos <39569926+santoseric@users.noreply.github.com>
2023-01-04 20:20:08 -0300

    Update main.go

--- main.go
+++ main.go
@@ -6,7 +6,7 @@ import (
 	"log"
 )
 
-const webContent = "dev-ops-ninja:v99"
+const webContent = "Test of automatic deployment on Kubernetes across Rancher :)"
 
 func main() {
 	http.HandleFunc("/", helloHandler)
